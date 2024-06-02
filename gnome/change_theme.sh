4a78dd11443a07f160f386dd9df8333487a8338f
Hoang Nguyen <hoangng71299@gmail.com>
2024-06-02 16:51:52 +0700

    feat(gnome/nightthemeswitcher): fix value

--- gnome/change_theme.sh
+++ gnome/change_theme.sh
@@ -24,7 +24,7 @@ fi
 THEME_SOURCE_DIR="$HOME/.themes/$theme_name/gtk-4.0"
 GTK_CONFIG_DIR="$HOME/.config/gtk-4.0"
 
-dconf write /org/gnome/shell/extensions/user-theme/name "$theme_name"
+dconf write /org/gnome/shell/extensions/user-theme/name "'$theme_name'"
 gsettings set org.gnome.desktop.interface color-scheme "$color_scheme"
 gsettings set org.gnome.desktop.interface cursor-theme "$cursor_theme"
 
